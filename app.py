5b077ecc111c973aad7780b464c0b1df891a3a97
chinrubber <admin@chinrubber.com>
2017-11-16 05:30:56 +0000

    Changes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import sys, os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand, command
+from eyespy.extensions import db
 
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 if BASE_DIR not in sys.path:
@@ -8,4 +11,14 @@ if BASE_DIR not in sys.path:
 
 from eyespy import create_app
 
-application = create_app()
+app = create_app()
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+
+with app.app_context():
+    command.upgrade(migrate.get_config(), 'head')
