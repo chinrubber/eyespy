7c0c560ef7953e1f02960117e8f3091532994494
chinrubber <admin@chinrubber.com>
2017-12-16 10:34:43 +0000

    Changes

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.join(os.path.dirname(__file__))
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
 
-app = create_app()
+application = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
