3279874e58a13a1e9c56dbdc056aae587456e1b2
chinrubber <admin@chinrubber.com>
2017-12-16 09:44:07 +0000

    Changes

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys, os
+from flask_migrate import Migrate, MigrateCommand, command
 
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 if BASE_DIR not in sys.path:
@@ -9,3 +10,8 @@ if BASE_DIR not in sys.path:
 from eyespy import create_app
 
 application = create_app()
+
+migrate = Migrate(app, db)
+
+with app.app_context():
+     command.upgrade(migrate.get_config(), 'head')
