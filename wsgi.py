03b91423556c809569e698a099cf0d972c1d9c3d
chinrubber <admin@chinrubber.com>
2017-12-16 10:22:37 +0000

    Changes

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from eyespy import create_app
+import os, sys
+
+BASE_DIR = os.path.join(os.path.dirname(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
 
 app = create_app()
 
