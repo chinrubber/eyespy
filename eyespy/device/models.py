8b3d1b0a9eb033e1e584bc4ecb1ff87487c080af
chinrubber <admin@chinrubber.com>
2017-12-18 05:58:16 +0000

    Added important field to device

--- eyespy/device/models.py
+++ eyespy/device/models.py
@@ -12,6 +12,7 @@ class Device(db.Model):
     vendor = Column(db.String(255), nullable=True, unique=False)
     hostname = Column(db.String(255), nullable=True, unique=False)
     lastseen = Column(db.DateTime(), nullable=False, unique=False, default=datetime.now().replace(microsecond=0))
+    important = Column(db.Boolean(), nullable=False, unique=False, default=False)
    
     def up(self):
         return (datetime.now() - self.lastseen).total_seconds() < 120
@@ -24,5 +25,6 @@ class Device(db.Model):
             'name': self.name,
             'vendor': self.vendor,
             'hostname': self.hostname,
+            'important': self.important,
             'up': self.up()
         }
